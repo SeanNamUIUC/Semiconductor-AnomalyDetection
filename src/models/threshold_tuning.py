598ffa2a399d2ff389005541087e922ee72a457d
SeanNamUIUC <shihyunnam7@gmail.com>
2026-05-04 05:00:26 +0000

    modified baseline_logistic.ipynb and threshold_tuning

--- src/models/threshold_tuning.py
+++ src/models/threshold_tuning.py
@@ -2,10 +2,12 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import precision_score, recall_score, f1_score
 
+#returns result of precision,recall and f1 values based on different thresholds
 def threshold_search(y_true, probs, thresholds):
     rows = []
 
     for th in thresholds:
+      #1 -> 확률이 >= th일때  pos_label: 불량, -1 -> neg_label: 정상
         pred = np.where(probs >= th, 1, -1)
 
         rows.append({
